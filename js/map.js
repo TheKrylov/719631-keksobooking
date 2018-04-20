7027c5d49d2fb907601e6a8445f1c1388ec21cc0
TheKrylov <thekrylov@gmail.com>
2018-04-20 22:13:24 +0300

    Refactoring

--- js/map.js
+++ js/map.js
@@ -116,6 +116,7 @@
 
     // add open event
     element.addEventListener('click', function () {
+      deleteCard();
       renderCard(pin);
     });
 
@@ -138,57 +139,93 @@
     adAddress.value = mapPinMain.style.left.slice(0, -2) + ', ' + mapPinMain.style.top.slice(0, -2);
   };
 
-  // Render card
-  var renderCard = function (pin) {
-
-    deleteCard();
-
+  var createCardTemplate = function () {
     userMap.insertBefore(mapCard.cloneNode(true), mapContainer);
+  };
 
-    var element = userMap.querySelector('.map__card');
+  var fillCard = function (element, pin) {
+    fillCardFields(element, pin);
+    fillCardFeatureList(element, pin);
+    fillCardPhotos(element, pin);
+  };
 
-
+  var fillCardFields = function (element, pin) {
     element.querySelector('.popup__title').textContent = pin.offer.title;
     element.querySelector('.popup__text--address').textContent = '';
     element.querySelector('.popup__text--price').textContent = pin.offer.price + '₽/ночь';
     element.querySelector('.popup__text--capacity').textContent = mock.typeRu[mock.type.indexOf(pin.offer.type)];
     element.querySelector('.popup__text--capacity').textContent = pin.offer.rooms + ' комнаты для ' + pin.offer.guests + ' гостей';
     element.querySelector('.popup__text--time').textContent = 'Заезд после ' + pin.offer.checkin + ' , выезд до ' + pin.offer.checkout;
+    element.querySelector('.popup__description').textContent = pin.offer.description;
+    element.querySelector('.popup__avatar').src = pin.author.avatar;
+  };
 
+  var fillCardFeatureList = function (element, pin) {
     var featuresList = element.querySelector('.popup__features');
 
-    for (var z = 0; z < mock.features.length; z++) {
-      var elementFeature = pin.offer.features.indexOf(mock.features[z]);
+    for (var i = 0; i < mock.features.length; i++) {
+      var featureElement = featuresList.querySelector('.popup__feature--' + mock.features[i]);
+      var isFeatureExist = pin.offer.features.indexOf(mock.features[i]) !== -1;
 
-      if (elementFeature < 0) {
-        featuresList.querySelector('.popup__feature--' + mock.features[z]).style.display = 'none';
+      if (isFeatureExist) {
+        showElement(featureElement);
       } else {
-        featuresList.querySelector('.popup__feature--' + mock.features[z]).style.display = 'inline-block';
+        hideElement(featureElement);
       }
     }
+  };
 
-    element.querySelector('.popup__description').textContent = pin.offer.description;
+  var setElementDisplayStyle = function (element, style) {
+    element.style.display = style;
+  };
 
+  var hideElement = function (el) {
+    setElementDisplayStyle(el, 'none');
+  };
+
+  var showElement = function (el) {
+    setElementDisplayStyle(el, 'inline-block');
+  };
+
+  var fillCardPhotos = function (element, pin) {
     var cardImages = element.querySelector('.popup__photos');
+
     cardImages.innerHTML = '';
+    cardImages.appendChild(generateCardPhotosElement(pin));
+  };
+
+  var generateCardPhotosElement = function (pin) {
+    var photosCount = pin.offer.photos.length;
     var fragmentPinImages = document.createDocumentFragment();
 
-    for (var p = 0; p < pin.offer.photos.length; p++) {
+    for (var i = 0; i < photosCount; i++) {
       var pinImage = mapCard.querySelector('.popup__photo').cloneNode(true);
-      pinImage.src = pin.offer.photos[p];
+      pinImage.src = pin.offer.photos[i];
       fragmentPinImages.appendChild(pinImage);
     }
 
-    cardImages.appendChild(fragmentPinImages);
+    return fragmentPinImages;
+  };
 
-    element.querySelector('.popup__avatar').src = pin.author.avatar;
-
-    // add close event
+  var addCardCloseHandler = function (element) {
     var close = element.querySelector('.popup__close');
     close.addEventListener('click', function () {
       deleteCard();
     });
+  };
 
+  var getCardElement = function () {
+    return userMap.querySelector('.map__card');
+  };
+
+  // Render card
+  var renderCard = function (pin) {
+    createCardTemplate();
+
+    var cardElement = getCardElement();
+
+    fillCard(cardElement, pin);
+    addCardCloseHandler(cardElement);
   };
 
   // Delete card
